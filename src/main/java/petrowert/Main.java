c315d5d80f2600db552c3f498bfe79ff9457ab99
unknown <petrowert1@gmail.com>
2021-05-07 18:45:42 +0600

    Add average monthly payment and etc

--- src/main/java/petrowert/Main.java
+++ src/main/java/petrowert/Main.java
@@ -18,42 +18,49 @@ import java.util.regex.Pattern;
 public class Main {
 	public static void main(String[] args) throws IOException {
 		Integer i, j;
-		String[] head_table = new String[15];
-    	String[] inter = new String[15];
-    	for(int a = 0; a < 15; a++)
-    		inter[a] = "0";
+		Double summ = 0.;
+    	Integer number_of_month = 12; // Original data count 13 month. From 092020 to 092020.
+    	// Last payment in data 30.09.2020
     	Integer num_cur = 10; // Number current month
-		Integer num_year = 2020; // Number current month
+		Integer num_year = 2020; // Current year
+		Integer period = 3;
 		Boolean y = false;
+		String[] head_table = new String[number_of_month + 3];
+    	String[] inter = new String[number_of_month + 3];
+    	for(int a = 0; a < (number_of_month + 3); a++)
+    		inter[a] = "0";
     	// Regular expression "\[[\d ]+:[\d \.]+];"
     	i = 2;
     	head_table[0] = "Number";
     	head_table[1] = "DZ";
-    	
-    	for(int k = 0; k < 13; k++) {
+    	head_table[2] = "MP"; // Average monthly payment. MP = (The amount of payments for the year)/12.
+    	for(int k = 0; k < number_of_month; k++) {
     		String zero;
-    		if((num_cur + k - 1) % 12 < 10)
+    		if((num_cur + k) % 12 < 10)
     			zero = "0";
     		else
     			zero = "";
     		if(y) {
-    			head_table[k + 2] = String.valueOf(num_year) + zero + String.valueOf((num_cur + k - 1) % 12);
+    			head_table[k + 3] = String.valueOf(num_year) + zero + String.valueOf((num_cur + k) % 12);
     			continue;
     		}
-    		if((num_cur + k - 1) % 12 == 0) {
+    		if((num_cur + k) % 12 == 0) {
     			y = true;
-    			head_table[k + 2] = String.valueOf(num_year) + String.valueOf(12);
+    			head_table[k + 3] = String.valueOf(num_year - 1) + String.valueOf(12);
     			continue;
     		}
-    		head_table[k + 2] = String.valueOf(num_year - 1) + zero + String.valueOf((num_cur + k - 1) % 12);
+    		head_table[k + 3] = String.valueOf(num_year - 1) + zero + String.valueOf((num_cur + k) % 12);
     	}
-    	FileWriter writer = new FileWriter("test_16k.csv");
+    	FileWriter writer = new FileWriter("data_only1group.csv");
     	for(String val: head_table) {
     		writer.append(val);
     		writer.append(';');
     	}
+    	writer.append("¹ group");
+		writer.append(';');
     	writer.append('\n');
-    	try (InputStream inputStream = new FileInputStream(new File("data_16k.xlsx"))) { //FilePath from your device
+    	System.out.println();
+    	try (InputStream inputStream = new FileInputStream(new File("data_cut_without_null.xlsx"))) { //FilePath from your device
 	        Workbook workbook = StreamingReader.builder().rowCacheSize(200).bufferSize(4096).open(inputStream);
 	        for (Sheet sheet : workbook) {
 	        	i = 0;
@@ -68,24 +75,33 @@ public class Main {
 	                    }
 	                    while (matcher.find()) {
 	                        String[] values = getStringCellValue(cell).substring(matcher.start(), matcher.end()).replace(" ", "").split(":");
-	                        for(int b = 2; b < 15; b++) {
+	                        for(int b = 3; b < (number_of_month + 3); b++) {
 	                        	if(values[0].equals(head_table[b])) {
 	                        		inter[b] = values[1];
+	                        		summ += Double.valueOf(values[1]);
 	                        	}
 	                        }
 	                    }
 	                	j++;
 	                }
-	                if(i > 0) {
-		                for(int a = 0; a < 15; a++) {
+	                // Check only 1 group of client
+	                //System.out.println(Double.valueOf(inter[1]) + "       " + summ/12*1.06);
+	                if(i > 0 && Double.valueOf(inter[1]) <= summ/12*1.06) {
+	                	inter[2] = String.valueOf(Math.round(summ/12 * 100.0) / 100.0); // String.valueOf(summ);
+		                for(int a = 0; a < (number_of_month + 3); a++) {
 		                	writer.append(inter[a]);
 		                	writer.append(';');
 		                }
+		                if(Double.valueOf(inter[1]) <= summ/12*1.06) {
+		                	writer.append("1");
+		                	writer.append(';');
+		                }	
 		                writer.append('\n');
+		                // Elements inter to zero.
+						for(int a = 0; a < (number_of_month + 3); a++)
+							inter[a] = "0";
+		            	summ = 0.;
 	                }
-	                // Elements inter to zero.
-					for(int a = 0; a < 15; a++)
-	            		inter[a] = "0";
 	                i++;
 	            }
 	        }
